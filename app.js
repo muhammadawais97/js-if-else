2d5adf9f19ec9ec8115de2637a366d56d5ef38e8
muhammad awais <154671272+muhammadawais97@users.noreply.github.com>
2024-03-03 22:25:12 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+//if and else PRACTICE///////////////////////////////////
+var userAge = +prompt("enter your age");
+
+if (userAge >= 18){
+    console.log("you can apply for nic");
+}
+else{
+    console.log("you are not eligibal");
+}
